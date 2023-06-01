8a1b4195372542e9500a96371fd35df3e9dada09
Marcus G. Oware <90935734+Weten-Ogen@users.noreply.github.com>
2023-06-01 11:02:53 +0000

    Update Requests.js

--- src/util/Requests.js
+++ src/util/Requests.js
@@ -4,7 +4,6 @@ const requests = {
     requestPopular:`https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
     requestTopRated:`https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
     requestTrending:`https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
-    requestHorror:`https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1`,
     requestUpcoming:`https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1&page=2`,
 
 }
