1ccd94eeab60d0051e59b92b37e2ead5c4a45c3a
Marcus G. Oware <90935734+Weten-Ogen@users.noreply.github.com>
2023-06-01 11:04:01 +0000

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,6 @@
 import React,{useState,useEffect} from 'react'
 import axios from 'axios';
 import requests from '../util/Requests';
-import {FaPlay} from 'react-icons/fa';
 
 const Main = () => {
     const [movies,setMovies] = useState([]);
@@ -45,17 +44,7 @@ const Main = () => {
             {movie?.title}
           </h1>
 
-          <div className='text-md md:text-lg my-6 flex items-center'>
-
-          <button 
-          className='flex items-center gap-2 border capitalize bg-gray-300 text-black border-gray-300 py-2  px-5'>
-            <p>play</p> 
-          <FaPlay/>
-          </button>
-          <button 
-          className='border capitalize  text-white border-gray-300 py-2 px-5 ml-4'>watch later
-          </button>
-          </div>
+         
 
           <p 
           className='text-gray-400  text-sm my-4'>
